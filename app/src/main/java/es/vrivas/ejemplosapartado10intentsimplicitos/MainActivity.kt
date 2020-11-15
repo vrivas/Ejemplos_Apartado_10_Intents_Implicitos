1d1397762fdb35e28849dac1d917961d8d48ad15
Victor Rivas <vrivas@ujaen.es>
2020-11-15 21:17:15 +0100

    Todos los intents implícitos funcionando, salvo el del vídeo (funciona en el móvil, no en el emulador)

--- app/src/main/java/es/vrivas/ejemplosapartado10intentsimplicitos/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado10intentsimplicitos/MainActivity.kt
@@ -1,14 +1,14 @@
 package es.vrivas.ejemplosapartado10intentsimplicitos
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,41 +17,121 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         im_browser.setOnClickListener(this)
         im_mail.setOnClickListener(this)
+        im_txt.setOnClickListener(this)
+        im_map.setOnClickListener(this)
+        im_contact.setOnClickListener(this)
+        im_youtube.setOnClickListener(this)
+    }
+
+    // Envía un intent a un navegador disponible en el dispositivo
+    fun intent_browser() {
+        try {
+            val uri = Uri.parse("https://duckduckgo.com/?q=Victor+Rivas+Santos")
+            val navegador =Intent(Intent.ACTION_VIEW, uri)
+            startActivity(navegador)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "No se pudo lanzar un navegador web: ${e.localizedMessage}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    // Envía un correo electrónico
+    fun intent_mail() {
+        try {
+            val datosMail: String = "mailto:vrivas@ujaen.es" +
+                    "?cc=victorrivassantos@gmail.com" +
+                    "&subject=" +
+                    Uri.encode("Estoy probando a enviar correos desde Android") +
+                    "&body=" +
+                    Uri.encode("Hola Víctor:\n Estoy enviando un correo. \n Saludos.")
+
+            val email = Intent(Intent.ACTION_SENDTO, Uri.parse(datosMail))
+            startActivity(email)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this, "No se puede enviar el email: ${e.localizedMessage}", Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    // Envía un mensaje de texto
+    fun intent_txt() {
+        try {
+            val mensaje="Hola. ¿Cómo estás?"
+
+            val texto = Intent(Intent.ACTION_SEND)
+            texto.setType("text/plain")
+            texto.putExtra(Intent.EXTRA_TEXT, mensaje)
+            startActivity(texto)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "No se puede enviar el mensaje de texto: ${e.localizedMessage}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    // Envía una geolocalización
+    fun intent_map() {
+        try {
+            val localidad="Jaen"
+            val uri=Uri.parse("geo:0,0?q=$localidad")
+            val mapa = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(mapa)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "No se puede mostrar el mapa: ${e.localizedMessage}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    // Llama a la aplicación YouTube
+    fun intent_youtube() {
+        try {
+            val app =
+                packageManager.getLaunchIntentForPackage("com.google.android.youtube")
+            startActivity(app)
+        } catch (e: Exception) {
+            Toast.makeText(
+                this,
+                "No se puede invocar a YouTube: ${e.localizedMessage}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    // Obtiene los datos de contacto
+    fun intent_contact() {
+        try {
+            val REQUEST_SELECT_CONTACT = 1
+
+            val contact = Intent(Intent.ACTION_PICK).apply {
+                type = ContactsContract.Contacts.CONTENT_TYPE
+            }
+            startActivityForResult(contact, REQUEST_SELECT_CONTACT)
+        } catch (e: Exception){
+            Toast.makeText(
+                this,
+                "No se puede acceder a los contactos: ${e.localizedMessage}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun onClick(v: View?) {
         when( v ) {
-            im_browser -> {
-                try {
-                    val navegador =
-                        Intent(Intent.ACTION_VIEW,
-                            Uri.parse("https://duckduckgo.com/?q=Victor+Rivas+Santos"))
-                    startActivity(navegador)
-                } catch (e: Exception) {
-                    Toast.makeText(
-                        this,
-                        "No se pudo lanzar un navegador web: ${e.localizedMessage}",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            } // im_browser
-            im_mail -> {
-                try {
-                    val datosMail: String = "mailto:vrivas@ujaen.es" +
-                            "?cc=victorrivassantos@gmail.com" +
-                            "&subject=" +
-                            Uri.encode("Estoy probando a enviar correos desde Android") +
-                            "&body=" +
-                            Uri.encode("Hola Víctor:\n Estoy enviando un correo. \n Saludos.");
-
-                    val email = Intent(Intent.ACTION_SENDTO, Uri.parse(datosMail));
-                    startActivity(email)
-                } catch( e: Exception ) {
-                    Toast.makeText(this
-                            , "No se puede enviar el email: ${e.localizedMessage}"
-                            , Toast.LENGTH_SHORT).show();
-                }
-            } // im_mail
+            im_browser -> intent_browser()
+            im_mail -> intent_mail()
+            im_txt -> intent_txt()
+            im_map -> intent_map()
+            im_youtube -> intent_youtube()
+            im_contact -> intent_contact()
         }
     }
 }
